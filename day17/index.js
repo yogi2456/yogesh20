f5e6252e6bec77fd20276c7c5c1638ca644015d0
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-08-12 21:28:14 +0530

    updates

---
+++ day17/index.js
@@ -0,0 +1,10 @@
+var myName = "yogesh";
+var mySurname = "sakluri";
+var myAge = "24";
+var Saturday = "saturday"
+var Saturday = "True"
+
+console.log(myName)
+console.log(mySurname)
+console.log(myAge)
+console.log(Saturady)
