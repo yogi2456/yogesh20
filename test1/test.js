94b75cc0b033156d89acf8203ac6d24358124413
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-08-26 18:35:39 +0530

    updates

---
+++ test1/test.js
@@ -0,0 +1,35 @@
+// var array = [1, 2, 3, 45, 65, 44, 22, 78, 56, 33, 89, 32];
+
+// for (var i = 0; i < array.length; i++) {
+//     if(array[i] % 2 == 0) {
+//         console.log(array[i], "this is even number")
+//     }
+// }
+
+
+
+var array1 = ["yogesh", "awdiz", "karan", "sagar"]
+var array2 = ["yogesh", "sagar", "mahesh", "ram"]
+
+for (var i = 0; i < array1.length; i++) {
+    //console.log(array1[i])
+    for (var j = 0; j < array2.length; j++) {
+        //console.log(array2[i])
+    }
+} 
+
+
+
+var array3 = ["yogesh", "awdiz", "karan", "sagar"]
+
+array3.push("mahesh")
+array3.push("ram")
+array3.push("yuvraj")
+
+console.log(array3)
+
+
+
+var array = [56, 76, 43, 52, 2, 4, 8, 69, 9, 6, 3];
+
+
