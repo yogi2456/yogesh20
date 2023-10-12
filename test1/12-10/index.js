fee93d5e0b46acb3753de0717ae65547f9a4facd
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-12 13:23:07 +0530

    updates

---
+++ test1/12-10/index.js
@@ -0,0 +1,84 @@
+// var age = 20;
+
+// if(age > 18) {
+//     console.log("you are older than 18")
+// } else {
+//     console.log("you are smaller than 18")
+// }
+
+// for( var i = 0; i <= 100; i++) {
+//     console.log(i)
+//     if(i == 50) {
+//         console.log("we got 50")
+//         break;
+//     }
+// }
+
+// var start = 23;
+// var end = 50;
+
+// for(var i = start; i <= end; i++) {
+//     if(i % 2 == 0) {
+//         console.log(`${i} - this is even number`)
+//     } else {
+//         console.log(`${i} - this is odd number`)
+//     }
+// }
+
+// var start = 10;
+// var end = 20;
+// var oddcount = 0;
+// var evencount = 0;
+
+// for(var i = start; i <= end; i++) {
+//     console.log(i)
+//     if(i % 2 != 0) {
+//         oddcount++;
+//     } else {
+//         evencount++;
+//     }
+// }
+
+// console.log(oddcount, "this is odd")
+// console.log(evencount, "this is even")
+
+// var result = 0;
+
+// for(var i = 1; i <= 5; i++) {
+//     if(i % 2 == 0) {
+//         console.log(i)
+//         result = result + i;
+//     }
+// }
+
+// console.log(result);
+
+
+// var number = 3;
+// var result = 1;
+
+// for(var i = 1; i <= number; i++) {
+//     result = result * i;
+// }
+
+// console.log(result);
+
+
+// var number = [1, 2, 3, 4, 5, 6, 7,];
+
+// console.log(number)
+// console.log(number[0])
+// console.log(number[1])
+// console.log(number[2])
+// console.log(number[3])
+// console.log(number[4])
+// console.log(number[5])
+// console.log(number[6])
+// console.log(number.length)
+
+var number = [1, 2, 3, 4, 5, 6, 7];
+
+for(var i = 0; i <= number.length; i++) {
+    console.log[number[i]]
+
+}
