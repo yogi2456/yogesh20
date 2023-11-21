d7887801179b259cec91dcf1bee01c99bd0a0a71
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-21 19:45:06 +0530

    updates

---
+++ karan/index.java
@@ -0,0 +1,8 @@
+class Main{
+public static void main(String [] args){
+    int age = 35;
+    System.out.println("before age:"+ age);
+    int age = 54;
+    System.out.println("after age:"+ age);
+     }
+  }
